505e8f14b630a2c85e08aa56494080e395eff21b
Muneer Iqbal <muneeriqbal2001@gmail.com>
2026-02-11 19:26:31 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1325,6 +1325,6 @@ init();
 
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("/service-worker.js")
+  navigator.serviceWorker.register("service-worker.js")
     .then(() => console.log("Service Worker Registered"));
 }
